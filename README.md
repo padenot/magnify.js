41499a97a47bb8fc1ee85a606fffb81414b92b55
Paul ADENOT <paul@paul.cx>
2011-10-19 13:34:24 +0200

    Release

---
+++ README.md
@@ -0,0 +1,30 @@
+# magnify.js, because sometimes, bigger is actually better
+
+A little script that magnify thumbnails, using CSS3 transitions and has no
+dependancies. It does one thing, but it does it well (I hope). Basically, just
+scratching my own itch, I was using FancyZoom in the past and it was buggy, slow
+as hell, and not modern at all. It worked on IE, though (but honestly, who cares ?).
+This has been tested on Firefox, Chromium and Opera, and works equally well on
+those browsers. It should work in any decent Webkit browser (like Safari).
+
+## Usage
+
+Drop that in your template, and all the images will be zoomable on click.
+```html
+<link type="text/css" rel="stylesheet" href="path/to/magnify.css">
+<script type="text/javascript" src="path/to/magnify.js"></script>
+<body onload="init_magnify()">
+```
+
+If you want an image not to be zoomable, add a `nomagnify` class to it :
+
+```html
+<img src="assets/img.jpg" alt="Example." title="Example." class="nomagnify">
+```
+
+## Found a bug ?
+Send a patch !
+
+## License
+
+New BSD
--- magnify.js
+++ magnify.js
@@ -11,59 +11,76 @@ function init_magnify() {
     return document.querySelectorAll(query);
   }
 
-  function createElement(type, className, html) {
-    var e = document.createElement(type);
-    e.className = className || "";
-    e.innerHTML = html || "";
-    return e;
+  function getOffset( el ) {
+    var _x = 0;
+    var _y = 0;
+    // walk up the dom
+    while( el != $('body')) {
+      _x += el.offsetLeft - el.scrollLeft;
+      _y += el.offsetTop - el.scrollTop;
+      el = el.offsetParent;
+    }
+    return { top: _y, left: _x };
   }
 
+  var original = null;
+  var magnified = null;
+
   function compute_scale_factor(img) {
-    var w = window.innerWidth * 0.9;
-    var h = window.innerHeight * 0.9;
+    // Magic number to put a little margin.
+    var w = window.innerWidth * 0.7;
+    var h = window.innerHeight * 0.7;
     var ratio_vert = h / img.height;
     var ratio_hor = w / img.width;
     return ratio_vert < ratio_hor ? ratio_vert : ratio_hor;
   }
 
-  var original = null;
-  var magnified = null;
+  function dispatch(e) {
+    if (e.keyCode == 27) {
+      demagnify_image($('.zoomed:not([inremoval=true])'));
+    }
+  }
+
   function demagnify_image(zoomed) {
     zoomed.setAttribute("inremoval", "true");
-    console.log("demagnify");
     zoomed.style.opacity = "0";
-    zoomed.style.width = original.width + "px";
-    zoomed.style.height = original.height + "px";
-    zoomed.style.left = original.offsetLeft + "px";
-    zoomed.style.top = original.offsetTop + "px";
+    if (original) {
+      var pos = getOffset(original);
+      zoomed.style.width = original.width + "px";
+      zoomed.style.height = original.height + "px";
+      zoomed.style.left = pos.left + "px";
+      zoomed.style.top = pos.top + "px";
+    }
     original = null;
     magnified = null;
     setTimeout(function() {
-      zoomed.parentNode.removeChild(zoomed);
+      if (zoomed.parentNode) {
+        zoomed.parentNode.removeChild(zoomed);
+      }
     }, 1000);
   }
 
   function magnify_image(e) {
-    console.log("magnify");
     // possibly demagnify other images
-    if (original == e.target) {
+    if (original !== null && original == e.target) {
       demagnify_image($('.zoomed:not([inremoval=true])'));
       return;
-    } else if (original !== null) {
+    } else if ($('.zoomed:not([inremoval=true])')) {
       demagnify_image($('.zoomed:not([inremoval=true])'));
     }
     if (magnified !== null) {
       demagnify_image(magnified);
     }
     var or = e.target;
-    var zoomed = createElement('img', 'zoomed');
+    var zoomed = document.createElement('img');
+    zoomed.className = "zoomed";
     zoomed.src = or.src;
+    var pos = getOffset(e.target);
     // Put the image about to be zoomed over the image
-    zoomed.style.position = "absolute";
     zoomed.style.width = or.width + "px";
     zoomed.style.height = or.height + "px";
-    zoomed.style.left = or.offsetLeft + "px";
-    zoomed.style.top = or.offsetTop + "px";
+    zoomed.style.left = pos.left + "px";
+    zoomed.style.top = pos.top + "px";
 
     $('body').appendChild(zoomed);
     zoomed.setAttribute("magnified", "true");
@@ -76,15 +93,22 @@ function init_magnify() {
     zoomed.style.height = original.height*ratio + "px";
     zoomed.style.opacity = "1";
     zoomed.style.left = window.innerWidth / 2 - original.width * ratio / 2 + "px";
-    zoomed.style.top = window.innerHeight / 2 - original.height * ratio / 2 + "px";
+    // center on viewport
+    zoomed.style.top = window.scrollY + window.innerHeight / 2 - or.height*ratio / 2 + "px";
     zoomed.addEventListener('click', function(e) {
       demagnify_image(e.target);
     }, false);
   }
 
-  var images = $$("img");
+  var images = $$("img:not(.nomagnify)");
   for(var i = 0; i < images.length; i++) {
     images[i].addEventListener("click", magnify_image, false);
+    images[i].style.cursor = "pointer";
   }
+
+  // Escape closes a magnified image.
+  document.addEventListener("keydown", function(e) {
+    dispatch(e);
+  }, false);
 }
 
